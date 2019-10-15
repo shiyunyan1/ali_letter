e027b57bd6a23109bf8dd83b3eb77439ef21d79a
shiyunyan <2678602339@qq.com>
2019-10-15 22:30:39 +0800

    111

--- src/router.js
+++ src/router.js
@@ -16,11 +16,12 @@ export default new Router({
     {
       path: "/about",
       name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+        import("./views/About.vue")
+    },
+    {
+      path: "/male",
+      component: () => import("@/components/Boyschannel.vue")
     }
   ]
 });
